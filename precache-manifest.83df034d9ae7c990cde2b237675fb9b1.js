d0f9a0df41b7bb0b51508ac2dee1ef740d1c1bfa
Anton Shein <adrior@yandex-team.ru>
2019-04-06 18:47:53 +0300

    Updates

---
+++ precache-manifest.83df034d9ae7c990cde2b237675fb9b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e8e4059c8ea3e3a8e38",
+    "url": "/tiktokmood/static/js/runtime~main.a824137e.js"
+  },
+  {
+    "revision": "743e2d3b680df2fce512",
+    "url": "/tiktokmood/static/js/main.02c41bbc.chunk.js"
+  },
+  {
+    "revision": "774d5e3722e17f42d013",
+    "url": "/tiktokmood/static/js/2.cba05053.chunk.js"
+  },
+  {
+    "revision": "743e2d3b680df2fce512",
+    "url": "/tiktokmood/static/css/main.c159aed3.chunk.css"
+  },
+  {
+    "revision": "a19e0c5e90964ff2a949843558229377",
+    "url": "/tiktokmood/index.html"
+  }
+];
